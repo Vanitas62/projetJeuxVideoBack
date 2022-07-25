2861f27566b30c9838f5c21c7743ed22fa1b6cd0
Vanitas <bryan.pauvre@isen-lille.fr>
2022-07-25 10:59:46 +0200

    First commit

---
+++ src/main/java/com/groupe3/projetJeuxVideoBack/ProjetJeuxVideoBackApplication.java
@@ -0,0 +1,13 @@
+package com.groupe3.projetJeuxVideoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJeuxVideoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJeuxVideoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe3/projetJeuxVideoBack/ProjetJeuxVideoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe3.projetJeuxVideoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJeuxVideoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
